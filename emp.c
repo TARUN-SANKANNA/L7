25acec8053b07197bc0435b3733d67a571d992ea
TARUN S S <tarun.s@s.amity.edu>
2025-05-03 07:55:39 +0530

    main function is added

---
+++ emp.c
@@ -0,0 +1,27 @@
+int main() {
+    int n, i;
+
+    // Ask how many employees
+    printf("Enter the number of employees: ");
+    scanf("%d", &n);
+
+    // Create array of Employee structures
+    struct Employee employees[n];
+
+    // Input employee details
+    for (i = 0; i < n; i++) {
+        printf("\nEnter details for Employee %d:\n", i + 1);
+        
+        printf("ID: ");
+        scanf("%d", &employees[i].id);
+
+        printf("Name: ");
+        scanf(" %[^\n]", employees[i].name); // To read full name including spaces
+
+        printf("Salary: ");
+        scanf("%f", &employees[i].salary);
+
+        printf("Department: ");
+        scanf(" %[^\n]", employees[i].department);
+    }
+
